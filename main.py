08b4ae0c6f637814baecfcf562fa90c08b6f94dc
Spiros Boutsis <spirosboutsis@hotmail.com>
2024-02-09 17:46:11 +0200

    Second Try

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 print('Welcome to our Restaurant')
 print('Book your table here')
+
+class Info:
+
+    def __init__(self, fname:str, lname:str, phone:int):
+        self.fname = fname
+        self.lname = lname
+        self.phone = phone
+
+class Action:
+
+    def __init__(self, fname, lname, phone):
+        self.info = Info(fname, lname, phone)
+        
