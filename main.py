d17e0a15be452a012b9f1427ad403d90ad6783dd
Spiros Boutsis <spirosboutsis@hotmail.com>
2024-02-12 12:32:09 +0200

    Add new reservation

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ print('Welcome to our Restaurant')
 print('Book your table here')
 import time
 class Info:
-
+#This class is used for getting the information of customer.
     def __init__(self, name:str, date: int, phone:int, num_persons: int):
         self.name = name
         self.date = date
         self.phone = phone
         self.num_persons = num_persons
-        #self.res_id = res_id
+        
 
     def print_info(self, name):
         if self.name == name:
@@ -20,7 +20,7 @@ class Info:
 
 
 class InsertReservation:
-
+#   Creating a class method that will allow users to add new reservations 
     def __init__(self):
         self.name = input('Name: ')
         self.date = (input('Date (DD/MM/YYYY): '))
@@ -29,7 +29,7 @@ class InsertReservation:
         self.num_persons = int(input('Number of persons: '))
     
         print('Attend for availability...')
-        time.sleep(5)
+        time.sleep(3)
         
         
 reservation =  InsertReservation()
