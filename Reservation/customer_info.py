ed9a0a17f35422beb9aa1b49af48b1e2447c6f8a
Spiros Boutsis <spirosboutsis@hotmail.com>
2024-02-19 16:46:42 +0200

    Function to get customer info

--- Reservation/customer_info.py
+++ Reservation/customer_info.py
@@ -1,2 +1,25 @@
+import sqlite3
+
+
+def db_conn():
+    connex = None
+    try:
+        connex = sqlite3.connect('Reservation_db')
+    except sqlite3.error as e:
+        print(e)
+    return connex
+
 def get_customer_info():
-    pass
+    connex = db_conn()
+    cursor = connex.cursor()
+
+    cursor.execute("SELECT * FROM Info WHERE LAST_NAME=?", (input('Enter last name: '),)) 
+    
+    data = cursor.fetchone()
+    if data is None:
+        print("No customer with that last name.")
+    else:
+        print('Yes the customer has a reservation.')
+
+
+get_customer_info()
